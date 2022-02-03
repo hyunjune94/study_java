7403e302332e60199a0e1dfd2a523345236d5e1f
강현준 <hyunjune94@naver.com>
2022-02-03 17:34:30 +0900

    ccc

--- src/com/mydomain/lecture/ch02/Hello.java
+++ src/com/mydomain/lecture/ch02/Hello.java
@@ -4,6 +4,15 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello~!~");
+		
+		int a = 1;
+		int b = 2;
+		
+		int c = 0;
+		
+		c = a + b;
+		
+		System.out.println(c);
 	}
 
 }
