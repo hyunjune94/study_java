4496bcb02ef2e8ea2ef0d1d69a9b27e2e20d4650
강현준 <hyunjune94@naver.com>
2022-02-04 17:21:50 +0900

    ㅊㅊㅊ

---
+++ src/com/mydomain/lecture/ch02/IntExample.java
@@ -0,0 +1,17 @@
+package com.mydomain.lecture.ch02;
+
+public class IntExample {
+
+	public static void main(String[] args) {
+
+		int var1 = 10;		//10진수 저장
+		int var2 = 012;		//8진수 저장
+		int var3 = 0xA;		//16진수 저장
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+
+	}
+
+}
---
+++ src/com/mydomain/lecture/ch02/LongExample.java
@@ -0,0 +1,23 @@
+package com.mydomain.lecture.ch02;
+
+public class LongExample {
+
+	public static void main(String[] args) {
+
+		long var1 = 10;
+		long var2 = 20L;
+		
+	//	컴파일 에러
+	//	int 범위를 벗어나는 리터럴인 경우에는 뒤에 'L'을 붙여주어야 된다
+	//	int : -2147483647 ~ 2147483647
+	//	long var3 = 100000000000; <- 에러
+		
+		long var4 = 100000000000L;
+
+		System.out.println(var1);
+		System.out.println(var2);
+	//	System.out.println(var3);
+		System.out.println(var4);
+	}
+
+}
