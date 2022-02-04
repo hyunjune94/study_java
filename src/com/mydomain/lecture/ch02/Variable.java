c559df11ff5ba2a0f598cb1d93e18efc9382e53b
강현준 <hyunjune94@naver.com>
2022-02-04 15:57:27 +0900

    cc

---
+++ src/com/mydomain/lecture/ch02/Variable.java
@@ -0,0 +1,59 @@
+package com.mydomain.lecture.ch02;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		
+	//	변수 명명법
+	//	첫글자 소문자 그리고 다음 문자열 부터는 대문자
+	//	대소문자 구분
+		
+	//	boolean defaultNy = 1;
+		String fullName = "Hyunjune Kang";
+		String name = "Hyunjune";
+				
+		int roomNumber = 1;
+		int roomnumber = 3;
+		
+		int a = 1;
+		int b = 2;		
+		int c = 0;
+		
+		c = a + b;		
+		
+		boolean defaultNy = false;
+		
+		byte aa = 0;
+		short bb = 0;
+		int cc = 0;
+		long dd = 0;
+		
+		float ee = 0.0f;
+		double ff = 0.0;
+		
+		char gg = ' ';
+		
+		String hh = " ";
+		
+		System.out.println("fullName: " + fullName);
+		System.out.println("name: " + name);
+		
+		System.out.println("roomNumber: " + roomNumber);
+		System.out.println("roomnumber: " + roomnumber);		
+		System.out.println("roomnumber: " + roomnumber + "!!!!");
+
+		System.out.println(c);
+		
+		System.out.println("한칸 들여쓰기");
+		System.out.println("\t한칸 들여쓰기");
+		
+		System.out.println("줄 바꿈");
+		System.out.println("\n줄 바꿈");
+		
+		System.out.println("\'나는 생각한다\'");
+		System.out.println("\"고로 존재한다\"");
+		System.out.println("\\");
+
+	}
+
+}
---
+++ src/com/mydomain/lecture/ch02/VariableExample.java
@@ -0,0 +1,14 @@
+package com.mydomain.lecture.ch02;
+
+public class VariableExample {
+
+	public static void main(String[] args) {
+		
+		int value = 10;
+		int result = value + 10;
+		
+		System.out.println("총 합은 : " + result);
+
+	}
+
+}
