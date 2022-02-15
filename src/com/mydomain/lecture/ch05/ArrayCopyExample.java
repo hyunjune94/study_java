cc84f5c7d5520c3cfc1c0b057ecdcab279e78aa8
강현준 <hyunjune94@naver.com>
2022-02-15 17:44:43 +0900

    ccccc

--- src/com/mydomain/lecture/ch05/ArrayCopyExample.java
+++ src/com/mydomain/lecture/ch05/ArrayCopyExample.java
@@ -1,23 +1,20 @@
 package com.mydomain.lecture.ch05;
 
-public class ArrayCopyByForExample {
+public class ArrayCopyExample {
 
 	public static void main(String[] args) {
-
 		int arr1[] = {1,2,3};
 		int arr2[] = new int[5];
 		
-		for(int i=0; i<arr1.length; i++) {
-			arr2[i] = arr1[i];
-		}
-		
-//		arr2[3] = 4;
-//		arr2[4] = 5;
+//		for(int i=0; i<arr2.length; i++) {
+//			System.out.println(arr2[i]);
+//		}
+
+		System.arraycopy(arr1, 0, arr2, 0, arr1.length);
 		
 		for(int i=0; i<arr2.length; i++) {
 			System.out.println(arr2[i]);
 		}
-		
 	}
 
 }
