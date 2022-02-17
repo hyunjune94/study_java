8f1030cab56b8360416526023fa46e7c237c1fe1
강현준 <hyunjune94@naver.com>
2022-02-17 17:42:19 +0900

    cc

---
+++ src/com/mydomain/lecture/ch06/Map.java
@@ -0,0 +1,15 @@
+package com.mydomain.lecture.ch06;
+
+public class Map {
+
+	String ifmmDeparture = "";
+	String ifmmDestination = "";
+	String ifmmTimeCost = "";
+	String direction = "";
+	String road = "";
+
+	int speed = 0;  
+	
+	
+	
+}
---
+++ src/com/mydomain/lecture/ch06/Member.java
@@ -0,0 +1,18 @@
+package com.mydomain.lecture.ch06;
+
+import java.util.Date;
+
+public class Member {
+	
+	String ifmm = "";
+	String ifmmName = "";
+	String ifmmId = "";
+	String ifmmPassword = "";
+	String address = "";
+	Date ifmmDob;
+	Integer ifmmAdminNy = 0;
+	Integer ifmmDelNy = 0;
+	Date ifmmRegDate;
+	Integer ifmmGenderCd = 0;	
+	
+}
