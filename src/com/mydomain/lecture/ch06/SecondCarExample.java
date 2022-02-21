234d0aa7565561c834d68fc7d4898ad2516bc7a5
강현준 <hyunjune94@naver.com>
2022-02-21 17:39:43 +0900

    ccc

--- src/com/mydomain/lecture/ch06/SecondCarExample.java
+++ src/com/mydomain/lecture/ch06/SecondCarExample.java
@@ -3,8 +3,40 @@ package com.mydomain.lecture.ch06;
 public class SecondCarExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		SecondCar secondCar = new SecondCar();		
+		
+		System.out.println("secondCar.company: " + secondCar.company);
+		System.out.println("secondCar.model: " + secondCar.model);
+		System.out.println("secondCar.color: " + secondCar.color);
+		System.out.println("secondCar.maxSpeed: " + secondCar.maxSpeed);
+
+		
+		SecondCar secondCar2 = new SecondCar("아반떼");
+		
+		System.out.println("-----------------------------------------");
+		System.out.println("secondCar2.company: " + secondCar2.company);
+		System.out.println("secondCar2.model: " + secondCar2.model);
+		System.out.println("secondCar2.color: " + secondCar2.color);
+		System.out.println("secondCar2.maxSpeed: " + secondCar2.maxSpeed);
+		
+		
+		SecondCar secondCar3 = new SecondCar("소나타", "blue");
+		
+		System.out.println("-----------------------------------------");
+		System.out.println("secondCar3.company: " + secondCar3.company);
+		System.out.println("secondCar3.model: " + secondCar3.model);
+		System.out.println("secondCar3.color: " + secondCar3.color);
+		System.out.println("secondCar3.maxSpeed: " + secondCar3.maxSpeed);
+		
+		
+		SecondCar secondCar4 = new SecondCar("그랜저", "black", 300);
+		
+		System.out.println("-----------------------------------------");
+		System.out.println("secondCar4.company: " + secondCar4.company);
+		System.out.println("secondCar4.model: " + secondCar4.model);
+		System.out.println("secondCar4.color: " + secondCar4.color);
+		System.out.println("secondCar4.maxSpeed: " + secondCar4.maxSpeed);
 	}
 
 }
