f48bfd531fd515040ab6eefed8d111c638523e70
강현준 <hyunjune94@naver.com>
2022-02-21 16:19:00 +0900

    c

---
+++ src/com/mydomain/lecture/ch06/FieldInitValue.java
@@ -0,0 +1,18 @@
+package com.mydomain.lecture.ch06;
+
+public class FieldInitValue {
+
+	byte byteValue;
+	short shortField;
+	int intField;
+	long longfield;
+	
+	boolean booleandField;
+	char charField;
+	
+	float floatField;
+	double doubleField;
+	
+	int[] arrField;
+	String referenceField;
+}
---
+++ src/com/mydomain/lecture/ch06/FieldInitValueExample.java
@@ -0,0 +1,21 @@
+package com.mydomain.lecture.ch06;
+
+public class FieldInitValueExample {
+
+	public static void main(String[] args) {
+
+		FieldInitValue fiv = new FieldInitValue();
+		
+		System.out.println("byteValue: " + fiv.byteValue);
+		System.out.println("shortField: " + fiv.shortField);
+		System.out.println("intField: " + fiv.intField);
+		System.out.println("longfield: " + fiv.longfield);
+		System.out.println("booleandField: " + fiv.booleandField);
+		System.out.println("charField: " + fiv.charField);
+		System.out.println("floatField: " + fiv.floatField);
+		System.out.println("doubleField: " + fiv.doubleField);
+		System.out.println("arrField: " + fiv.arrField);
+		System.out.println("referenceField: " + fiv.referenceField);
+	}
+
+}
