c4853cdac68c129620087510d626eb7a3a90151b
강현준 <hyunjune94@naver.com>
2022-03-16 17:37:15 +0900

    ccc

---
+++ src/com/mydomain/lecture/ch15/ArraysAsListExample.java
@@ -0,0 +1,22 @@
+package com.mydomain.lecture.ch15;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArraysAsListExample {
+
+	public static void main(String[] args) {
+		List<String> list1 = Arrays.asList("강현준", "전지현", "장동건");
+		for(String name: list1) {
+			System.out.println(name);
+		}
+		
+		List<Integer> list2 = Arrays.asList(1, 2, 3);
+		for(int value: list2) {
+			System.out.println(value);
+		}
+		
+
+	}
+
+}
